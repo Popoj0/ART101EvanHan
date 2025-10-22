ec47cbcda6beb6cae2a6c981b015019316adeb95
evan <evjhan@ucsc.edu>
2025-10-22 08:36:13 -0700

    fix?

--- art101fresh/exercises/ex04.js
+++ art101fresh/exercises/ex04.js
@@ -1,21 +1,32 @@
 
 
-let count = 0;
-let colors= ["Orchid", "Coral", "HotPink", "Plum"];
+var count = 0;
+let colorCount=0;
+var colors = ["Orchid", "Coral", "HotPink", "Plum"]; 
 
-$("#needy-button").click( function() {
 
-    $("#needy-button").html("Clicks: " + count + " Color: " + colors[count] );
-    
-    $("body").css("background-color"), colors[reminder]);
+$("#needy-button").click(function () {
 
-    count = count + 1
+    if ( count < 5) { mood = "gresh and happy" ; }
+    else if ( (count >=5) && (count <10 )) { mood="keep pushing"; }
+    else { mood = "so tired" ; }
+
+    $("#needy-button").html( "Clicks: " + count + " Color: " + colors[colorCount] + " " + mood); 
+
+    $("body").css("background-color", colors[colorCount]);
+
+    if ( colors[colorCount] == "Orchid" ) {
+        $("body").append("<img width=50 src='images/orchid.png'>");
+}
+
+
+
+    count = count + 1;
+    colorCount = colorCount + 1;
+
+    if ( colorCount == 4 ) {  colorCount=0;  }
+
+
+
 
 });
-
-
-
-
-
-
-
