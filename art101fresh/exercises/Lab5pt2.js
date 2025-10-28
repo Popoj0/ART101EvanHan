a9524126ed9c38c4eca730c5534bb2d776a1d4c5
evan <evjhan@ucsc.edu>
2025-10-28 11:53:25 -0700

    lab 5

---
+++ art101fresh/exercises/Lab5pt2.js
@@ -0,0 +1,23 @@
+$("#output").append( "hello" );
+$("#boudy").append( "We are in a forest, watch out for ghosts!");
+$("#talking").append( "Tucked in Evan's cozy forest cabin, Jhon wakes to birds doing karaoke and pine trees gossiping in the breeze. He brews a mug of something warm, pokes the friendly fire, and steps outside like it's his tiny kingdom. No traffic, no buzz, just moss, mugs, and mischief. Forest life suits him just fine.")
+
+
+
+$(document).ready(function(){
+    $("#good-button").click(function(){
+        $("#output1").text('Good morning! Time for Jhon to start his day by chopping trees! ');
+  
+
+    });
+    $("#bad-button").click(function(){
+        $("#output1").text("Good night! Jhon is tucking himself to sleep, shhhh!")
+
+
+    });
+
+})
+
+
+
+
